9b2c8132095a36d4f955d3c795ffed40111315d9
projectaudiostream <projectaudiostream@gmail.com>
2026-01-05 17:42:44 +0530

    get-alll-details-done

--- src/components/AppSidebarNav.js
+++ src/components/AppSidebarNav.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import { CBadge } from "@coreui/react";
+import axios from "axios";
+import { isAutheticated } from "src/auth";
 
 export const AppSidebarNav = ({ items }) => {
-
   const location = useLocation();
+  let token=isAutheticated()
+  const [loading,setLoading]=useState(false)
+  const [accessData,setAccessData]=useState([])
+
+
+  
+  const handlegetAllData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/v1/user/login/", {
+    
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("response?.data", response?.data?.data);
+      setAccessData(response?.data);
+    } catch (error) {
+      const errormssage = error.response && error.response.data.message;
+      console.log("errormssage", errormssage);
+    } finally {
+      setLoading(false);
+    }
+  };
   const navLink = (name, icon, badge) => {
     return (
-      <div className="d-flex" style={{margin:"-7px 0px"}}>
+      <div className="d-flex" style={{ margin: "-7px 0px" }}>
         {icon && icon}
         {name && name}
         {badge && (
@@ -26,42 +51,25 @@ export const AppSidebarNav = ({ items }) => {
     const Component = component;
     return (
       <>
-        {/* <Component
-        // className="bg-info"
+        <Component
           {...(rest.to &&
             !rest.items && {
               component: NavLink,
-         
-              activeclassname: "active",
+              end: false,
+              className: ({ isActive }) => {
+                const highlightList = rest.highlightPaths || [];
+                const shouldHighlight = highlightList.some((path) =>
+                  location.pathname.includes(path)
+                );
+
+                return isActive || shouldHighlight ? "active" : "";
+              },
             })}
           key={index}
           {...rest}
         >
           {navLink(name, icon, badge)}
-        </Component> */}
-   <Component
-  {...(rest.to &&
-    !rest.items && {
-      component: NavLink,
-      end: false,
-      className: ({ isActive }) => {
-        const highlightList = rest.highlightPaths || [];
-        const shouldHighlight = highlightList.some(path =>
-          location.pathname.includes(path)
-        );
-
-        return isActive || shouldHighlight ? "active" : "";
-      },
-    })}
-  key={index}
-  {...rest}
->
-  {navLink(name, icon, badge)}
-</Component>
-
-
-
-    
+        </Component>
       </>
     );
   };
@@ -82,7 +90,11 @@ export const AppSidebarNav = ({ items }) => {
       </Component>
     );
   };
+useEffect(()=>{
+  handlegetAllData()
+},[])
 
+console.log("accessData",accessData)
   return (
     <React.Fragment>
       {items &&
